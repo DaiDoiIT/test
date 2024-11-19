a709218e0c86b448127b1c7e7c88c62d9fbdfb64
Nguyen Thai An <AnNTTH03911@fpt.edu.vn>
2024-11-19 15:01:48 +0700

    Even and odd number formatted

--- src/test/Test.java
+++ src/test/Test.java
@@ -18,14 +18,17 @@ public class Test {
         System.out.println("This is a commit");
         System.out.println("Hey");
         System.out.println("New clone");
+        System.out.println("\nSố chẵn:");
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
-            if (i % 2 != 0) {
-                System.out.println(i);
+        }
+        System.out.println("\nSố lẻ:");
+        for (int j = 0; j < 10; j++) {
+            if (j % 2 != 0) {
+                System.out.println(j);
             }
         }
     }
-    
 }
