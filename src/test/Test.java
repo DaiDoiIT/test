72958e49793510b1e5d4770da77e363b44692e42
Nguyen Thai An <AnNTTH03911@fpt.edu.vn>
2024-11-19 14:51:05 +0700

    Checking even number

--- src/test/Test.java
+++ src/test/Test.java
@@ -18,6 +18,11 @@ public class Test {
         System.out.println("This is a commit");
         System.out.println("Hey");
         System.out.println("New clone");
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.err.println(i);
+            }
+        }
     }
     
 }
