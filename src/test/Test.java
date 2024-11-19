7d92c5cb78754167d602f1dc04843b87f512cefa
Nguyen Thai An <AnNTTH03911@fpt.edu.vn>
2024-11-19 14:52:43 +0700

    Checking odd number

--- src/test/Test.java
+++ src/test/Test.java
@@ -20,7 +20,10 @@ public class Test {
         System.out.println("New clone");
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
-                System.err.println(i);
+                System.out.println(i);
+            }
+            if (i % 2 != 0) {
+                System.out.println(i);
             }
         }
     }
