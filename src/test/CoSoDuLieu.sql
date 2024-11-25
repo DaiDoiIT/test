1b94d32f124818d4140c72b947a407569e9274fa
Nguyen Thai An <AnNTTH03911@fpt.edu.vn>
2024-11-25 08:55:07 +0700

    Xác định yêu cầu chức năng và phân tích nghiệp vụ

--- src/test/CoSoDuLieu.sql
+++ src/test/CoSoDuLieu.sql
@@ -3,4 +3,9 @@ CREATE TABLE NhanVien(
     ten VARCHAR(255) PRIMARY KEY NOT NULL,
     Ma VARCHAR(100) not null,
     SDT int NOT NULL
-)
+);
+
+insert into NhanVien VALUES
+('Nguyen Ba Quang', 'NV01', 0901564812);
+
+select * from NhanVien
