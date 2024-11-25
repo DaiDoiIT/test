e47efdff67d84df82fe2bb6b6c79da25eb724be1
Nguyen Thai An <AnNTTH03911@fpt.edu.vn>
2024-11-25 11:04:05 +0700

    An test

---
+++ src/test/Test.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author vanh
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int number = 10;
+
+       if (number % 2 == 0) {
+
+           System.out.println(number + " là số chẵn.");
+
+       } else {
+
+           System.out.println(number + " là số lẻ.");
+
+       }
+        System.out.println("JJJ");
+        System.out.println("A commit");
+        System.out.println("Lê Tuấn Anh");
+    }
+    
+}
