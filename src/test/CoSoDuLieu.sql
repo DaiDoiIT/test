58825678c4531c86d4c617d04f46632361765d87
Nguyen Thai An <AnNTTH03911@fpt.edu.vn>
2024-11-25 08:59:36 +0700

    Quản lý lỗi và xử lý ngoại lệ

--- src/test/CoSoDuLieu.sql
+++ src/test/CoSoDuLieu.sql
@@ -6,6 +6,10 @@ CREATE TABLE NhanVien(
 );
 
 insert into NhanVien VALUES
-('Nguyen Ba Quang', 'NV01', 0901564812);
+('Nguyen Ba Quang', 'NV01', 0901564812),
+('Cao Ba Quat', 'NV02', 0981374123),
+('Nguyen Minh Quang', 'NV03', 0971263456);
 
 select * from NhanVien
+
+select ten from NhanVien where Ma = 'NV01';
