13b17e129da5fd6e64f5143da2a30aace12c8a06
Tuan Anh <tl255666@gmail.com>
2024-11-26 21:23:08 +0700

    Tuan Anh

--- src/test/Test.java
+++ src/test/Test.java
@@ -10,14 +10,34 @@ package test;
  */
 public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("JJJ");
-        System.out.println("A commit");
-        System.out.println("Lê Tuấn Anh");
+   import java.util.Scanner;
+
+public class FactorialCalculator {
+
+    // Method to calculate factorial using recursion
+    public static long factorial(int n) {
+        if (n <= 1) {
+            return 1; // Base case: factorial of 0 or 1 is 1
+        }
+        return n * factorial(n - 1); // Recursive call
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            long result = factorial(number);
+            System.out.println("The factorial of " + number + " is: " + result);
+        }
+
+        scanner.close();
+    }
+}
+
     
 }
