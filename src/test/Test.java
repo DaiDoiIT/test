22f2365959dbe07b3d2be83c16df060f0609174d
Hong Minh <minhdhth02328@fpt.edu.vn>
2024-11-27 10:49:39 +0700

    Hong Minh

--- src/test/Test.java
+++ src/test/Test.java
@@ -17,10 +17,24 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("JJJ");
-        System.out.println("JJJ");
-        System.out.println("Đỗ Hồng Minh");
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số thứ nhất: ");
+        int number1 = scanner.nextInt(); // Đọc số nguyên thứ nhất
+
+        System.out.print("Nhập số thứ hai: ");
+        int number2 = scanner.nextInt(); // Đọc số nguyên thứ hai
+
+        // Tính tổng hai số
+        int sum = number1 + number2;
+
+        // Hiển thị kết quả
+        System.out.println("Tổng của " + number1 + " và " + number2 + " là: " + sum);
+
+        // Đóng Scanner để tránh rò rỉ tài nguyên
+        scanner.close();
+    }
         
     }
 
-}
+
