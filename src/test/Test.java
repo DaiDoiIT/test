c5e72585a15ed32e3ecaefb0fb02ad8bc9bd8b4b
Tuan Anh <tl255666@gmail.com>
2024-11-27 10:38:48 +0700

    Tuan Anh

--- src/test/Test.java
+++ src/test/Test.java
@@ -16,36 +16,67 @@ public class Test {
 
     public static void main(String[] args) {
 
+        ArrayList<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        int choice;
 
-        System.out.print("Enter a positive integer: ");
-        int number = scanner.nextInt();
+        do {
+            System.out.println("\n--- To-Do List Menu ---");
+            System.out.println("1. Add a Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Remove a Task");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
 
-        if (number < 0) {
-            System.out.println("Factorial is not defined for negative numbers.");
-        } else {
-            long result = factorial(number);
-            System.out.println("The factorial of " + number + " is: " + result);
-        }
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter a new task: ");
+                    String task = scanner.nextLine();
+                    tasks.add(task);
+                    System.out.println("Task added!");
+                    break;
+                case 2:
+                    System.out.println("\nYour Tasks:");
+                    if (tasks.isEmpty()) {
+                        System.out.println("No tasks yet!");
+                    } else {
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println((i + 1) + ". " + tasks.get(i));
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("\nYour Tasks:");
+                    if (tasks.isEmpty()) {
+                        System.out.println("No tasks to remove!");
+                    } else {
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println((i + 1) + ". " + tasks.get(i));
+                        }
+                        System.out.print("Enter the task number to remove: ");
+                        int taskNumber = scanner.nextInt();
+                        if (taskNumber > 0 && taskNumber <= tasks.size()) {
+                            tasks.remove(taskNumber - 1);
+                            System.out.println("Task removed!");
+                        } else {
+                            System.out.println("Invalid task number!");
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Exiting... Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 4);
 
         scanner.close();
-
-        // TODO code application logic here
-         int number = 10;
-
-       if (number % 2 == 0) {
-
-           System.out.println(number + " là số chẵn.");
-
-       } else {
-
-           System.out.println(number + " là số lẻ.");
-
-       }
         System.out.println("JJJ");
         System.out.println("A commit");
         System.out.println("Lê Tuấn Anh");
-        Viet anh test
 
     }
 
