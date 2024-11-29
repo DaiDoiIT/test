fca3d74a8d067bb62d05fc9e44eb627b690e3450
Nguyen Thai An <AnNTTH03911@fpt.edu.vn>
2024-11-29 21:47:37 +0700

    Quản lý phiên kết nối và thời gian sống của token

--- src/test/CoSoDuLieu.sql
+++ src/test/CoSoDuLieu.sql
@@ -25,3 +25,7 @@ select * from NhanVien where ten = 'Viet Anh';
 select * from NhanVien LIMIT 5;
 select ten from KhachHang LIMIT 10;
 select Ma from NhanVien where ten = 'Viet Anh';
+
+-- Token COMMENT
+select ten, KhachHang.ten from NhanVien AS NV
+GROUP BY KhachHang on KhachHang.ten = NV.ten;
