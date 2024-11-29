39909a4055911de83bd2a210a78007a0dd554574
Vanhhh2727 <va2732007@gmail.com>
2024-11-29 21:56:53 +0700

    Viet Anh

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package test;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
@@ -19,6 +22,33 @@ public class Test {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
+
+        JFrame frame = new JFrame("Giao Diện Đơn Giản");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+
+        // Tạo một nhãn (label)
+        JLabel label = new JLabel("Chào mừng bạn đến với Java GUI!", JLabel.CENTER);
+
+        // Tạo một nút (button)
+        JButton button = new JButton("Nhấn vào đây");
+
+        // Định nghĩa hành động khi nút được nhấn
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Cập nhật nhãn khi nút được nhấn
+                label.setText("Nút đã được nhấn!");
+            }
+        });
+
+        // Đặt bố cục và thêm các thành phần vào khung
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.add(label);
+        frame.add(button);
+
+        // Hiển thị cửa sổ
+        frame.setVisible(true);
     }
     
 }
