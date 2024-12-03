a87e7f0095c5c3e0bda46f9f565982233cdd0d6f
Hong Minh <minhdhth02328@fpt.edu.vn>
2024-12-03 23:26:35 +0700

    Hong Minh

--- src/test/Test.java
+++ src/test/Test.java
@@ -498,3 +498,15 @@ public class SumTwoNumbers {
     }
 }
 
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Bảng cửu chương từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Bảng cửu chương " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println(); // Dòng trống giữa các bảng
+        }
+    }
+}
